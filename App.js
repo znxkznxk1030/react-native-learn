9a2fd6a689b917edce9e8ca0d5eddf5354991cf9
ysKim <znxkznxk1030@gmail.com>
2021-11-18 07:33:39 +0530

    adjust prettier

--- App.js
+++ App.js
@@ -3,18 +3,26 @@ import React, { useState } from 'react';
 import { Button, StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
-
-  const [outputText, setOutputText] = useState('Open up App.js to start working');
+  const [outputText, setOutputText] = useState(
+    'Open up App.js to start working'
+  );
 
   return (
-    <View style={{padding: 35}}>
-      <View>
-        <TextInput placeholder="Course Goal" style={{ borderColor: 'black', borderWidth: 1, padding: 10 }}/>
+    <View style={{ padding: 50 }}>
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+        }}
+      >
+        <TextInput
+          placeholder="Course Goal"
+          style={{ borderColor: 'black', borderWidth: 1, padding: 10 }}
+        />
         <Button style={styles.button} title="ADD" />
       </View>
-      <View>
-
-      </View>
+      <View></View>
     </View>
   );
 }
@@ -29,5 +37,5 @@ const styles = StyleSheet.create({
   button: {
     backgroundColor: '#fff',
     color: 'blue',
-  }
+  },
 });
